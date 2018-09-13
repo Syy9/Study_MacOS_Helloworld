962da87e7202919ae95b595ecb1bd66ac4c2438a
Syy <shirakawa.yy@gmail.com>
2018-09-13 22:40:34 +0900

    Initial Commit

---
+++ Study_MacOS_Helloworld/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Study_MacOS_Helloworld
+//
+//  Created by Syy on 2018/09/13.
+//  Copyright © 2018年 syy. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Study_MacOS_Helloworld/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Study_MacOS_Helloworld
+//
+//  Created by Syy on 2018/09/13.
+//  Copyright © 2018年 syy. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
